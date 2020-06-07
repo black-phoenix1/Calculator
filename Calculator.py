82c61029e1f30fbc19d49a5df73a11e72a96e619
Lucretius Biah <63173187+lucre-god@users.noreply.github.com>
2020-06-07 01:09:57 +0000

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -5,16 +5,19 @@ import time
 import easter_egg
 import view_menu
 import help_menu
+import edit_menu
+
+
 
 root = Tk() #sets the root to Tk
-root.title("Calculator 0.0.0.1")
-
+root.title("Calculator 0.0.1")
+frame = LabelFrame(root, borderwidth = 5)
 
 menubar = Menu(root)
 root.config(menu = menubar)
 
 #Code for display screen
-display = Entry(root, width = 35, borderwidth = 5)
+display = Entry(frame, width = 35, borderwidth = 5)
 display.grid(row = 0 , column = 0, columnspan = 4, ipady = 15) #Screen configuration
 
 
@@ -46,7 +49,11 @@ def click(object):
 
 #Function for DEL
 def backspace(): #Delete only one character from the input display
-    display.delete(0,1)
+    number = display.get()
+    then = number[:-1]
+    display.delete(0,END)
+    display.insert(0,then)
+
 
 #Function for AC
 def AC():
@@ -82,19 +89,24 @@ def equal():
         display.insert(0, "Syntax Error")
         button_del.configure(state = "disabled")
         display.configure(state = "disabled")
+    except NameError:
+        display.delete(0, END)
+        display.insert(0, "Syntax Error")
+        button_del.configure(state="disabled")
+        display.configure(state="disabled")
 
 
 #Codes for number buttons
-button_1 = Button(root, text = "1", command = lambda : click(1), padx = 20, pady = 20)
-button_2 = Button(root, text = "2", command = lambda : click(2), padx = 20, pady = 20)
-button_3 = Button(root, text = "3", command = lambda : click(3), padx = 20, pady = 20)
-button_4 = Button(root, text = "4", command = lambda : click(4), padx = 20, pady = 20)
-button_5 = Button(root, text = "5", command = lambda : click(5), padx = 20, pady = 20)
-button_6 = Button(root, text = "6", command = lambda : click(6), padx = 20, pady = 20)
-button_7 = Button(root, text = "7", command = lambda : click(7), padx = 20, pady = 20)
-button_8 = Button(root, text = "8", command = lambda : click(8), padx = 20, pady = 20)
-button_9 = Button(root, text = "9", command = lambda : click(9), padx = 20, pady = 20)
-button_0 = Button(root, text = "0", command = lambda : click(0), padx = 20, pady = 20)
+button_1 = Button(frame, text = "1", command = lambda : click(1), padx = 20, pady = 20)
+button_2 = Button(frame, text = "2", command = lambda : click(2), padx = 20, pady = 20)
+button_3 = Button(frame, text = "3", command = lambda : click(3), padx = 20, pady = 20)
+button_4 = Button(frame, text = "4", command = lambda : click(4), padx = 20, pady = 20)
+button_5 = Button(frame, text = "5", command = lambda : click(5), padx = 20, pady = 20)
+button_6 = Button(frame, text = "6", command = lambda : click(6), padx = 20, pady = 20)
+button_7 = Button(frame, text = "7", command = lambda : click(7), padx = 20, pady = 20)
+button_8 = Button(frame, text = "8", command = lambda : click(8), padx = 20, pady = 20)
+button_9 = Button(frame, text = "9", command = lambda : click(9), padx = 20, pady = 20)
+button_0 = Button(frame, text = "0", command = lambda : click(0), padx = 20, pady = 20)
 
 #Gridding system for number buttons
 
@@ -110,17 +122,17 @@ button_9.grid(row = 3 , column = 2)
 button_0.grid(row = 4 , column = 0)
 
 #Codes for symbols
-button_point = Button(root, text = ".",padx =22, pady = 20, command = lambda : click("."))
-button_bracket1 = Button(root, text = "(",padx =22, pady = 20, command = lambda : click("("))
-button_bracket2 = Button(root, text = ")",padx =22, pady = 20, command = lambda : click(")"))
-button_ac= Button(root, text = "AC", fg = "red",padx =15, pady = 20 , command = AC)
-button_del = Button(root, text = "DEL", fg = "red",padx =14, pady = 20 ,command = backspace)
-button_plus = Button(root, text = "+", padx =18, pady = 20, command = lambda : click('+'))
-button_minus = Button(root, text = "-", padx = 20, pady = 20, command = lambda : click("-"))
-button_divide = Button(root, text = "/", padx = 20, pady = 20, command = lambda : click("/"))
-button_add = Button(root, text = "+", padx = 20, pady = 20, command = lambda : click("+"))
-button_multiply = Button(root, text = "*", padx = 20, pady = 20 , command = lambda :click("*"))
-button_equal = Button(root, text = "=", fg = "green", pady = 20, padx = 19, command = equal)
+button_point = Button(frame, text = ".",padx =22, pady = 20, command = lambda : click("."))
+button_bracket1 = Button(frame, text = "(",padx =22, pady = 20, command = lambda : click("("))
+button_bracket2 = Button(frame, text = ")",padx =22, pady = 20, command = lambda : click(")"))
+button_ac= Button(frame, text = "AC", fg = "red",padx =15, pady = 20 , command = AC)
+button_del = Button(frame, text = "DEL", fg = "red",padx =14, pady = 20 ,command = backspace)
+button_plus = Button(frame, text = "+", padx =19, pady = 20, command = lambda : click('+'))
+button_minus = Button(frame, text = "-", padx = 20.500, pady = 20, command = lambda : click("-"))
+button_divide = Button(frame, text = "÷", padx = 19.400, pady = 20, command = lambda : click("/"))
+button_add = Button(frame, text = "+", padx = 22, pady = 20, command = lambda : click("+"))
+button_multiply = Button(frame, text = "×", padx = 19, pady = 20 , command = lambda :click("*"))
+button_equal = Button(frame, text = "=", fg = "green", pady = 20, padx = 19, command = equal)
 #Gridding system for symbols
 button_bracket1.grid(row = 4, column = 1)
 button_bracket2.grid(row = 4, column = 2)
@@ -135,7 +147,11 @@ button_multiply.grid(row = 4, column = 3)
 button_equal.grid(row = 5, column = 3)
 
 
-root.resizable(width = "False", height ="False")
+
 view_menu.main(root, menubar)
+edit_menu.main(root, menubar)
 help_menu.main(root, menubar)
+
+frame.grid(row =0, column = 0)
+root.resizable(width= False, height= False)
 root.mainloop() #End of the program
--- view_menu.py
+++ view_menu.py
@@ -1,14 +1,16 @@
 from tkinter import *
+
 """This is the part of the program which describes the view meunu"""
+
+
 class View:
-	def view(menubar):
-		view_menu  = Menu(menubar, tearoff = 0)
-		view_menu.add_command(label = "Algebraic Expressions")
-		view_menu.add_command(label = "Shapes and Figures")
-		view_menu.add_separator()
-		view_menu.add_command(label = "Unit Conversions")
-		view_menu.add_command(label = "Date Calculations")
-		menubar.add_cascade(label  ="View", menu = view_menu)
+    def date(root):
+        date_frame = LabelFrame(root, background = "light green")
+        label = Label(date_frame, text = "Select the type of date calculation").grid(row = 0, column = 0)
+        clicked = StringVar()
+
+        drop_down = OptionMenu(date_frame, clicked, "Calculate difference between dates","Add or subtract to a specified date         ").grid(row = 1, column = 0)
+        date_frame.grid(row =0, column = 1)
 
 
 
@@ -18,14 +20,25 @@ class View:
 
 
 
-def main(root,menubar):
-	
-	View.view(menubar)
-	
-	
 
 
+
+
+    def view(menubar, root):
+        view_menu = Menu(menubar, tearoff=0)
+        view_menu.add_command(label="Basic mode")
+        view_menu.add_separator()
+        view_menu.add_command(label="Algebraic Expressions")
+        view_menu.add_command(label="Shapes and Figures")
+        view_menu.add_separator()
+        view_menu.add_command(label="Unit Conversions")
+        view_menu.add_command(label="Date Calculations", command = lambda : View.date(root))
+        menubar.add_cascade(label="View", menu=view_menu)
+
+
+def main(root, menubar):
+    View.view(menubar, root)
 
 
 if __name__ == "__main__":
-	print("Please run main")
+    print("Please run main")
